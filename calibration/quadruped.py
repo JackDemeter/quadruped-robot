18b184150b193cc976b0ccfa037c82cfb9aae9d8
jackDemeter <jack.demeter@queensu.ca>
2021-11-20 15:47:53 -0500

    added inverse kinematics tests

--- calibration/quadruped.py
+++ calibration/quadruped.py
@@ -3,6 +3,7 @@
 from adafruit_servokit import ServoKit
 from enum import IntEnum
 import time
+import math
 
 
 class Motor(IntEnum):
@@ -21,8 +22,11 @@ class Motor(IntEnum):
 class Quadruped:
     def __init__(self):
         self.kit = ServoKit(channels=16)
+        self.upper_leg_length = 10
+        self.lower_leg_length = 10
         for i in range(10):
             self.kit.servo[i].set_pulse_width_range(500,2500)
+        
         
     def calibrate(self):
         # set the robot into the default "middle position" use this for attaching legs in right location
@@ -36,7 +40,6 @@ class Quadruped:
         self.kit.servo[Motor.BR_ELBOW].angle = 90
         self.kit.servo[Motor.BL_SHOULDER].angle = 120
         self.kit.servo[Motor.BL_ELBOW].angle = 90
-            
 
     def set_shoulders(self, offset, anti=[]):
         #TODO ADD MIN MAX POSITIONS!!!
@@ -97,6 +100,33 @@ class Quadruped:
 
     def set_angle(self,leg_id, degrees):
         self.kit.servo[leg_id].angle = degrees
+
+    def test_pos(self,leg_id, x,y):
+        a1 = self.upper_leg_length
+        a2 = self.lower_leg_length
+
+        c2 = (x**2+y**2-a1**2-a2**2)/(2*a1*a2)
+        s2 = math.sqrt(1-c2**2)
+        theta2 = math.atan2(s2,c2)
+        c2 = math.cos(theta2)
+        s2 = math.sin(theta2)
+
+        c1 = (x*(a1+(a2*c2)) + y*(a2*s2))/(x**2+y**2)
+        s1 = (y*(a1+(a2*c2)) - x*(a2*s2))/(x**2+y**2)
+        theta1 = math.atan2(s1,c1)
+        # generate positions with respect to robot motors
+        theta_shoulder = -theta1
+        theta_elbow = theta_shoulder - theta2
+        theta_shoulder = 180 - self.rad_to_degree(theta_shoulder)
+        theta_elbow = 130 - self.rad_to_degree(theta_elbow)
+        self.kit.servo[Motor.FR_SHOULDER].angle = theta_shoulder
+        self.kit.servo[Motor.FR_ELBOW].angle = theta_elbow
+        print("theta shoulder:",theta_shoulder,"\ttheta_elbow:",theta_elbow)
+        return [theta_shoulder, theta_elbow]
+
+    def rad_to_degree(self,rad):
+        return rad*180/math.pi
+
 
 if __name__ == "__main__":
     r = Quadruped()
