9e5cb4eebd1c060eb3a76e7e91942d45c8f45608
jackDemeter <jack.demeter@queensu.ca>
2021-12-01 17:44:40 -0500

    added hip movement

--- calibration/quadruped.py
+++ calibration/quadruped.py
@@ -42,32 +42,44 @@ class Quadruped:
     def set_angle(self,leg_id, degrees):
         self.kit.servo[leg_id].angle = degrees
 
-    def test_pos(self,x,y,shoulder,elbow,right=True):
+    def test_pos(self,shoulder,elbow,x,y,z=0,hip=None,right=True):
+        L=2
+        y_prime = -math.sqrt((z+L)**2 + y**2)
+        thetaz = math.atan2(z+L,abs(y))-math.atan2(L,abs(y_prime))
+        print(thetaz/math.pi*180)
+
         elbow_offset = 20
         shoulder_offset = 10
         a1 = self.upper_leg_length
         a2 = self.lower_leg_length
 
-        c2 = (x**2+y**2-a1**2-a2**2)/(2*a1*a2)
+        c2 = (x**2+y_prime**2-a1**2-a2**2)/(2*a1*a2)
         s2 = math.sqrt(1-c2**2)
         theta2 = math.atan2(s2,c2)
         c2 = math.cos(theta2)
         s2 = math.sin(theta2)
 
-        c1 = (x*(a1+(a2*c2)) + y*(a2*s2))/(x**2+y**2)
-        s1 = (y*(a1+(a2*c2)) - x*(a2*s2))/(x**2+y**2)
+        c1 = (x*(a1+(a2*c2)) + y_prime*(a2*s2))/(x**2+y_prime**2)
+        s1 = (y_prime*(a1+(a2*c2)) - x*(a2*s2))/(x**2+y_prime**2)
         theta1 = math.atan2(s1,c1)
         # generate positions with respect to robot motors
         theta_shoulder = -theta1
         theta_elbow = theta_shoulder - theta2
+        theta_hip = 0
         if right:
             theta_shoulder = 180 - self.rad_to_degree(theta_shoulder) + shoulder_offset
             theta_elbow = 130 - self.rad_to_degree(theta_elbow) + elbow_offset
+            if hip:
+                theta_hip = 90 - self.rad_to_degree(thetaz)
         else:
             theta_shoulder = self.rad_to_degree(theta_shoulder) - shoulder_offset
             theta_elbow = 50 + self.rad_to_degree(theta_elbow) - elbow_offset
+            if hip:
+                theta_hip = 90 + self.rad_to_degree(thetaz)
         self.kit.servo[shoulder].angle = theta_shoulder
         self.kit.servo[elbow].angle = theta_elbow
+        if hip:
+            self.kit.servo[hip].angle = theta_hip
         # print("theta shoulder:",theta_shoulder,"\ttheta_elbow:",theta_elbow)
         return [theta_shoulder, theta_elbow]
 
@@ -86,7 +98,7 @@ class Quadruped:
             [-15.0, -15.0],
         ])
         curve = bezier.Curve(slide_nodes, degree=1)
-        s_vals = np.linspace(0.0, 1.0, 60)
+        s_vals = np.linspace(0.0, 1.0, 20)
         slide = curve.evaluate_multi(s_vals)
 
         foot_motion = step
@@ -99,19 +111,63 @@ class Quadruped:
         y = foot_motion[1]
         
         for cycle in range(cycles):
+<<<<<<< HEAD
             for index in range(80):
-                self.test_pos(x[index],y[index],Motor.FR_SHOULDER,Motor.FR_ELBOW,right=True)
-                self.test_pos(x[(index+20)%80],y[(index+20)%80],Motor.BR_SHOULDER,Motor.BR_ELBOW,right=True)
-                self.test_pos(x[(index+40)%80],y[(index+40)%80],Motor.FL_SHOULDER,Motor.FL_ELBOW,right=False)
-                self.test_pos(x[(index+60)%80],y[(index+60)%80],Motor.BL_SHOULDER,Motor.BL_ELBOW,right=False)
+                self.test_pos(Motor.FR_SHOULDER,Motor.FR_ELBOW,x[index],y[index],right=True)
+                self.test_pos(Motor.BR_SHOULDER,Motor.BR_ELBOW,x[(index+20)%80],y[(index+20)%80],right=True)
+                self.test_pos(Motor.FL_SHOULDER,Motor.FL_ELBOW,x[(index+40)%80],y[(index+40)%80],right=False)
+                self.test_pos(Motor.BL_SHOULDER,Motor.BL_ELBOW,x[(index+60)%80],y[(index+60)%80],right=False)
                 time.sleep(0.02)
+=======
+            for index in range(40):
+                self.test_pos(x[index],y[index],Motor.FR_SHOULDER,Motor.FR_ELBOW,right=True)
+                self.test_pos(x[(index+20)%40],y[(index+20)%40],Motor.BR_SHOULDER,Motor.BR_ELBOW,right=True)
+                self.test_pos(x[(index+20)%40],y[(index+20)%40],Motor.FL_SHOULDER,Motor.FL_ELBOW,right=False)
+                self.test_pos(x[index],y[index],Motor.BL_SHOULDER,Motor.BL_ELBOW,right=False)
+                time.sleep(0.01)
+>>>>>>> 6df16d3... tuning and 2 step
 
     def rad_to_degree(self,rad):
         return rad*180/math.pi
+
+    def test_turn(self,cycles=1):
+        # Generate foot tragectory
+        step_nodes = np.asfortranarray([
+            [-4.0, -4.0, 0.0, 0.0],
+            [-15.0, -10, -10, -15.0],
+        ])
+        curve = bezier.Curve(step_nodes, degree=3)
+        s_vals = np.linspace(0.0, 1.0, 20)
+        step = curve.evaluate_multi(s_vals)
+
+        slide_nodes = np.asfortranarray([
+            [0.0, -4.0],
+            [-15.0, -15.0],
+        ])
+        curve = bezier.Curve(slide_nodes, degree=1)
+        s_vals = np.linspace(0.0, 1.0, 20)
+        slide = curve.evaluate_multi(s_vals)
+
+        turn_motion = step
+        for x,y in zip(slide[0],slide[1]):
+            new_x=np.append(turn_motion[0],x)
+            new_y=np.append(turn_motion[1],y)
+            turn_motion = [new_x,new_y]
+
+        z = turn_motion[0]
+        y = turn_motion[1]
+        for cycle in range(cycles):
+            for index in range(40):
+                    self.test_pos(Motor.FR_SHOULDER,Motor.FR_ELBOW,0,y[index],z=z[index],hip=Motor.FR_HIP,right=True)
+                    self.test_pos(Motor.BR_SHOULDER,Motor.BR_ELBOW,0,y[(index+20)%40],right=True)
+                    self.test_pos(Motor.FL_SHOULDER,Motor.FL_ELBOW,0,y[(index+20)%40],z=-z[(index+20)%40],hip=Motor.FL_HIP,right=False)
+                    self.test_pos(Motor.BL_SHOULDER,Motor.BL_ELBOW,0,y[(index)%40],right=False)
+                    time.sleep(0.02)
+
 
 
 if __name__ == "__main__":
     r = Quadruped()
     r.calibrate()
     time.sleep(1)
-    r.test_step(5)
+    r.test_turn(5)
